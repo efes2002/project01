22871e07ee2b415bcd15832704ab54e2408af144
Kirill Zhidkov <efes2002@mail.ru>
2020-05-21 01:55:32 +0400

    настройка стилей

--- src/App.js
+++ src/App.js
@@ -4,8 +4,32 @@ import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            Привет
+        <div className="app">
+            <div className="back"></div>
+            <header className="app-header">
+
+
+            </header>
+            <nav className="app-nav">
+                <ol>
+                    <li>
+                        <a href="/">
+                            <span className="left_icon f1"></span>
+                            <span className="left_label">Профиль</span>
+                        </a>
+
+
+                    </li>
+                    <li>
+                        <span className="left_icon f2"></span>
+                        <span className="left_label">Сообщения</span>
+                    </li>
+                </ol>
+            </nav>
+            <div className="app-content">
+                body
+            </div>
+
         </div>
     );
 }
